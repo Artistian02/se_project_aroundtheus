90babc9b39d6cf4cc49eae157add4db0cefa680c
Artistian02 <artistian02@gmail.com>
2023-04-07 15:29:27 -0400

    update

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+const initialCards = [
+    {
+      name: "Yosemite Valley",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    },
+    {
+      name: "Lake Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    },
+    {
+      name: "Vanoise National Park",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    },
+  ];
+  console.log(initialCards);
+  
+  const profileEditButton = document.querySelector("#profile-edit-button");
+  const profileEditModal = document.querySelector("#profile-edit-modal");
+  profileEditButton.addEventListener("click", () => {
+    profileEditModal.classList.add("modal__opened");
+  });
+  const editCloseButton = document.querySelector("#edit-close-button");
+  const editCloseModal = document.querySelector("#profile-edit-modal");
+  editCloseButton.addEventListener("click", () => {
+    editCloseModal.classList.remove("modal__opened");
+  });
